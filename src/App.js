6946e06aa55f05363abd2c59cb0d689fd75eeb55
bronnyhub <julka.bronny@gmail.com>
2023-01-29 18:33:12 +0100

    1.0
    first time with react
    still trying to get used to it

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <div className="navbar">
+      <img src='snail.png' className="snail-logo"></img>
+      <p className='text'>snailsGram</p>
+     </div>
+
+    <div className='gallery'>
+      <img src='snail1.jpg' className='snails'></img>
+      <img src='snail2.jpg' className='snails'></img>
+      <img src='snail3.jpg' className='snails'></img>
+
+    </div>
+
+    </div>
+  );
+}
+
+export default App;
