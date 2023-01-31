bb239fe3ae6e8dbf14100c6ccee901ba44739b39
bronnyhub <julka.bronny@gmail.com>
2023-01-31 07:45:16 +0100

    1.1
    added some navbar buttons
    changed font
    
    TO ADD NEXT: button functions

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
      <div className="navbar">
-      <img src='snail.png' className="snail-logo"></img>
+      <img src='snail.png' className="snail-logo" alt='snails-logo'></img>
       <p className='text'>snailsGram</p>
+      <button className='btn'>MAIN</button>
+      <button className='btn'>BACKGROUND</button>
+      <button className='btn'>CONTACT</button>
      </div>
 
     <div className='gallery'>
-      <img src='snail1.jpg' className='snails'></img>
-      <img src='snail2.jpg' className='snails'></img>
-      <img src='snail3.jpg' className='snails'></img>
-
+      <img src='snail1.jpg' className='snails' alt='snails'></img>
+      <img src='snail2.jpg' className='snails' alt='snails'></img>
+      <img src='snail3.jpg' className='snails' alt='snails'></img>
     </div>
 
     </div>
